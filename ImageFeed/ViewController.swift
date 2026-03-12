c2e5f74ed6e259b6a314ac7329afcf532736125b
Alexandr Butygin <a.butygin@gmail.com>
2026-03-12 17:33:31 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by ALEXANDER BUTYGIN on 12.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
