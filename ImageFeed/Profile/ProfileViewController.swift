7a9838f514a24df338dc54411d7a3d0c98a508ac
Alexandr Butygin <butyginav@psbank.ru>
2026-04-03 16:40:32 +0300

    sprint_09. Profile screen - WIP. SingleImage controller and UINavigationController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by ALEXANDER BUTYGIN on 03.04.2026.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private weak var avatarImage: UIImageView!
+    @IBOutlet private weak var exitButton: UIButton!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var loginLabel: UILabel!
+    @IBOutlet private weak var statusLabel: UILabel!
+
+    @IBAction func exitButtonTouched() {
+    }
+}
